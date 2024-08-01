1a29c08d036249458878ee7ed9bd26a519235c0f
vica-t <128752278+vica-t@users.noreply.github.com>
2024-08-01 13:48:24 +0300

    Delete idk.py

--- idk.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://www.ted.com/talks?sort=popular&topics%5B%5D=education"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
